8f9899303f04891574f970fdffb2f8c2ba210d18
katakamrupendra <katakamrupendra@gmail.com>
2026-04-01 20:45:52 +0530

    feat: implement MainNode class to initialize ROS 2 node functionality

--- ur5_control/src/main.cpp
+++ ur5_control/src/main.cpp
@@ -1,5 +1,12 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MainNode : public rclcpp::Node{
+    public:
+        MainNode() : Node("main_node"){
+            RCLCPP_INFO(this->get_logger(),"main node started");
+        }
+}
+    
 int main(int argc, char **argv){
     rclcpp::init(argc, argv);
     rclcpp::shutdown();
